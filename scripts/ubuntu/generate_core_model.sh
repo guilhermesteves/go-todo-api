215f538aa1f2af2efd6567710d2c5e2939bbd7dc
Guilherme Esteves <guilhermesteves@icloud.com>
2019-05-29 04:11:20 -0300

    fix scripts

--- scripts/ubuntu/generate_core_model.sh
+++ scripts/ubuntu/generate_core_model.sh
@@ -1,2 +1,2 @@
-protoc --go_out=../../../../ --proto_path=../api/core/proto/ ../api/core/proto/model/*.proto
-protoc --go_out=../../../../ --proto_path=../api/core/proto/ ../api/core/proto/message/*.proto
+protoc --go_out=../../../../../ --proto_path=../../api/core/proto/ ../../api/core/proto/model/*.proto
+protoc --go_out=../../../../../ --proto_path=../../api/core/proto/ ../../api/core/proto/message/*.proto
